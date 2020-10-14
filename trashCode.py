a1f2d25857ecc2644b22e101ef7759734ea7d4ed
Aldrin Brillante <aldrinbrillante@Aldrins-MacBook-Pro.local>
2020-10-14 15:17:02 -0700

    created trashCode file in case I need to use old code that I removed from main

--- trashCode.py
+++ trashCode.py
@@ -1,6 +1,12 @@
-#this is code versions of different parts of my project progress
 
+##########################################################################################################
+
+# TRASH CODE 
+# this file is not used in the program. It is more so in case i need to use old code I created
+# again if I mess up during my progress
 # this is the function that acts when user responds to being 'male' onto terminal
+
+###########################################################################################################
 def male():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
